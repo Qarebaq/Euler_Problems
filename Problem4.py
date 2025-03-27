5bfd4f323610818629d0986847b1d1d8263e60c0
Amir Mohammad Mohammadi <amirmohammad.gharebagh1385@gmail.com>
2025-03-27 04:40:23 +0330

    p4

--- Problem4.py
+++ Problem4.py
@@ -1,13 +1,38 @@
-num = 600851475143
-maximal =0
-temp =0
-i = 2
-for i in range(2,int(num**0.5)+1):
-    if num%i ==0:
-        for j in range(2,i):
-            if i%j == 0:
-                temp = temp +1
-        if temp == 0:
-            maximal = i
+def is_Palindrome(num):
+    temp1 = int(num)
+    temp = int(num) 
+    Answer = int(num)
+    n = 0
+    Check = 0 
+    while num> 0:
+        num = num // 10
+        n = n + 1
+    arr = [0] * n
+    arrtst = [0] * n
+    for i in range(0,n):
+        arr[i]  = temp %10 
+        temp = int(temp // 10)
+    for j in range(n-1,-1,-1):
+        arrtst[j] = temp1 % 10
+        temp1 = int(temp1 // 10)
+    for i in range(0,n):
+        if (arr[i] == arrtst[i]):
+            Check = Check + 1
+    if (Check == n):
+        Check_is_this_Big(Answer)
 
-print(maximal)
+def Check_is_this_Big(answer):
+    global Biggest_palindrome  
+    if answer > Biggest_palindrome:
+        Biggest_palindrome = answer
+
+
+Biggest_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if is_Palindrome(product) and product > Biggest_palindrome:
+            Biggest_palindrome = product
+
+print("The biggest palindrome is:", Biggest_palindrome)
